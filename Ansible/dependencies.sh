bda136e3f8ccad8ab46cc856038f71ae10f22725
Matt Sokol <maciej.sokol79@gmail.com>
2020-12-17 10:19:09 +0000

    Tests fixed and added dependencies

---
+++ Ansible/dependencies.sh
@@ -0,0 +1,32 @@
+sudo apt install python3 -y
+sudo apt install python3-pip -y
+sudo pip install --upgrade pip -y
+
+python3 -m pip install awscli
+python3 -m pip install boto
+python3 -m pip install boto3
+python3 -m pip install atomicwrites==1.3.0
+python3 -m pip install attrs==19.1.0
+python3 -m pip install beautifulsoup4==4.8.0
+python3 -m pip install certifi==2019.6.16
+python3 -m pip install chardet==3.0.4
+python3 -m pip install idna==2.8
+python3 -m pip install importlib-metadata==0.19
+python3 -m pip install more-itertools==7.2.0
+python3 -m pip install packaging==19.1
+python3 -m pip install -U pluggy
+python3 -m pip install py==1.8.0
+python3 -m pip install pyparsing==2.4.2
+python3 -m pip install pytest==5.1.0
+python3 -m pip install requests==2.22.0
+python3 -m pip install six==1.12.0
+python3 -m pip install soupsieve==1.9.2
+python3 -m pip install urllib3==1.25.3
+python3 -m pip install wcwidth==0.1.7
+python3 -m pip install zipp==0.5.2
+python3 -m pip install Click==7.0
+python3 -m pip install Flask==1.0.2
+python3 -m pip install itsdangerous==1.1.0
+python3 -m pip install Jinja2==2.10
+python3 -m pip install MarkupSafe==1.1.1
+python3 -m pip install Werkzeug==0.15.5
